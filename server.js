bf54c2d2e15524719bd7211f26dbf6b743648730
Diana Stebbins <dianastebbins97@gmail.com>
2020-05-05 16:24:53 -0700

    adding all the service worker stuff from master

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+// prep for Heroku port usage
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,10 +16,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// first part for heroku link, third param uses new mongoose
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget",
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false
+  });
 
 // routes
 app.use(require("./routes/api.js"));
